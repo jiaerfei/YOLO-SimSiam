f3a18dbd4fe526e5745c34a50db8e0ffc8a833dd
jiaerfei <51914854+jiaerfei@users.noreply.github.com>
2026-06-09 10:35:12 +0800

    Update box_main.py

--- box_main.py
+++ box_main.py
@@ -1,25 +1,21 @@
 import argparse
 import os
 import cv2
-
 import numpy as np
+
 from ultralytics import YOLO
-from ultralytics.utils.ops import scale_image
-from ultralytics.utils.plotting import Annotator
 
 from SSL_main import YOLOSimSiam
-
-from utils import resize_to_show
 
 
 parser = argparse.ArgumentParser("YOLO detection training only for GPUs")
 # data
-parser.add_argument("--data", default=r"C:\jiafeidl\YOLO_model_local\cfg\rock_data.yaml",
+parser.add_argument("--data", default=r" ",
                     type=str, help="data root yaml path")
 parser.add_argument("--batch-size", default=4, type=int, help="img nums for all gpus")
 parser.add_argument("--input-size", default=640, type=int, help="image size for training")
 # model
-parser.add_argument("--pretrained", default="/home/featurize/work/YOLO_model/knn_contra/0.01_SGD_cos/weights/epoch200.pt",
+parser.add_argument("--pretrained", default=" ",
                     type=str, help="pretrained checkpoint path")
 parser.add_argument("--frozen", default=9, type=int, help="freeze n layers (backbone) to train")
 # train
@@ -43,14 +39,14 @@ if __name__ == "__main__":
     ps = parser.parse_args()
 
     # normal train
-    # model = YOLO(model="yolo11s-seg.yaml", task="segment", verbose=True)
+    # model = YOLO(model="yolo11n-seg.yaml", task="segment", verbose=True)
     # train_result = model.train(data=ps.data, epochs=ps.epochs, imgsz=ps.input_size, batch=ps.batch_size,
     #                            name=ps.name, project=ps.project, patience=ps.patience,
     #                            optimizer=ps.optim, lr0=ps.lr0, cos_lr=ps.cos_lr, lrf=ps.lrf)
 
 
     # few train
-    # model = YOLO(model="yolo11s-seg.yaml", task="segment", verbose=True).load(ps.pretrained)
+    # model = YOLO(model="yolo11n-seg.yaml", task="segment", verbose=True).load(ps.pretrained)
     # train_result = model.train(data=ps.data, epochs=ps.epochs, imgsz=ps.input_size, batch=ps.batch_size,
     #                            name=ps.name, project=ps.project, patience=ps.patience,
     #                            optimizer=ps.optim, lr0=ps.lr0, cos_lr=ps.cos_lr, lrf=ps.lrf,
